f01ed5e09c2d90f6c21eba11937dd7bc3f13b57d
yatashashank <yatashashank@gmail.com>
2018-09-21 16:10:30 -0500

    set up start script for heroku port

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
 
+const port = process.env.PORT || 2000;
 var app = express();
 
 hbs.registerPartials(__dirname + '/views/partials')
@@ -22,9 +23,9 @@ app.use((req, res, next) => {
     next();
 });
 
-app.use((req, res, next) => {
-    res.render('maintenance.hbs')
-})
+//app.use((req, res, next) => {
+//res.render('maintenance.hbs')
+//})
 
 //HBS helper functions
 hbs.registerHelper('getCurrentYear', () => {
@@ -52,4 +53,6 @@ app.get('/bad', (req, res) => {
         errorMessage: 'Cannot fetch Sorry',
     })
 });
-app.listen(2000);
+app.listen(port, () => {
+    console.log(`Server running on ${port}`)
+});
